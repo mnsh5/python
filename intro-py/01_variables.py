0161797d0ab80103e6a048402ab446a5bc387bd1
mnsh4 <a779433@DESKTOP-HUPN93P>
2024-01-29 12:10:39 -0300

    :rocket: add new files

---
+++ intro-py/01_variables.py
@@ -0,0 +1,6 @@
+country = "Argentina"
+print(country)
+
+PI = 3.14159
+RADIANS_TO_DEGREES = 180 / PI
+print(RADIANS_TO_DEGREES)
---
+++ intro-py/02_numbers.py
@@ -0,0 +1,14 @@
+num = 35 # Integer
+PI = 3.14159 # Float
+
+maths_operation = 1 + 3 * 4 / 2 - 2
+print(maths_operation)
+
+float_division = 12 / 3
+print(float_division)
+
+integer_division = 8 // 3 # (//) elimina los decimales
+print(integer_division)
+
+remainder = 13 % 5
+print(remainder)
